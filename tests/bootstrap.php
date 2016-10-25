17c19747b9387831a7180b668303ca64ba2058f5
Carsten Witt <carsten.witt@gmail.com>
2016-10-25 12:35:26 +0200

    Improve autloading

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -12,5 +12,3 @@ $autoloader_file = __DIR__ . '/../vendor/autoload.php';
 if (!is_readable( $autoloader_file )) {
     die( PHP_EOL . "Missing Composer's vendor/autoload.php; run 'composer update' first." . PHP_EOL . PHP_EOL );
 }
-$autoloader = require $autoloader_file;
-$autoloader->addPsr4('tests\\',  __DIR__.'/src');
