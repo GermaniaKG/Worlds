1c4cff2aeef96a55cc0cb50bac7df15ecc5bede4
Webdev Walter <412560+tomkyle@users.noreply.github.com>
2022-06-30 16:08:07 +0200

    Slight modernizing

--- src/World.php
+++ src/World.php
@@ -4,35 +4,35 @@ namespace Germania\Worlds;
 class World extends WorldAbstract implements WorldInterface
 {
 
-    public function setId( $id )
+    public function setId( $id ) : self
     {
         $this->id = $id;
         return $this;
     }
 
 
-    public function setName( $name )
+    public function setName( $name ) : self
     {
         $this->name = $name;
         return $this;
     }
 
 
-    public function setSlug( $slug )
+    public function setSlug( $slug ) : self
     {
         $this->slug = $slug;
         return $this;
     }
 
 
-    public function setDescription( $description )
+    public function setDescription( $description ) : self
     {
         $this->description = $description;
         return $this;
     }
 
 
-    public function setPhoto( $photo )
+    public function setPhoto( $photo ) : self
     {
         $this->photo = $photo;
         return $this;
